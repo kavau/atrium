2e72ffb16e31e49f17851293ca906ddfcfce7ffb
kavau <kavaunix@proton.me>
2026-04-27 10:38:51 +0200

    Tools: a helper that launches a session on a given seat

--- TODO.md
+++ TODO.md
@@ -1,10 +1,8 @@
 # Next steps
 
-- VT handling for seat0
 - Fork a session helper
 - Session lifetime management: monitor SIGCHLD and restart session/greeter
 - Greeter
 - Seat discovery
 - Hotplug monitoring
 - Write a proper PAM service file
-- Turn the current main.c into a diagnostic binary that can launch hardcoded sessions on hardcoded seats
---
+++ tools/start-session.c
@@ -0,0 +1,49 @@
+/*
+ * start-session.c - starts a user session on a given seat. Intended for testing
+ * and development.
+ *
+ * Usage (with local PAM configuration):
+ *     sudo systemd-run --scope ./build/atrium-start-session <username> <seat> data/pam
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "daemon/core/session.h"
+#include "daemon/core/vt.h"
+
+int main(int argc, char *argv[]) {
+    if (argc < 3 || argc > 4) {
+        fprintf(stderr, "Usage: %s <username> <seat> [conf_path]\n", argv[0]);
+        return 1;
+    }
+
+    const char *username = argv[1];
+    const char *seat = argv[2];
+    const char *conf_path = argc == 4 ? argv[3] : NULL;
+    const char *password = getpass("Password: ");
+    if (!password) {
+        fprintf(stderr, "getpass failed\n");
+        return 1;
+    }
+
+    /* Allocate a VT for seat0. */
+    int vtnr = 0;
+    if (strcmp(seat, "seat0") == 0) {
+        vtnr = vt_alloc();
+        if (vtnr < 0) {
+            fprintf(stderr, "Failed to allocate VT: %d\n", vtnr);
+            return EXIT_FAILURE;
+        }
+        /* TODO: suppress VT keyboard so keystrokes typed into the Wayland
+        session don't leak into the TTY's input buffer. */
+    }
+
+    int r = create_session(username, password, seat, vtnr, conf_path);
+    if (vtnr > 0) {
+        vt_release(vtnr);
+    }
+    return r;
+}
