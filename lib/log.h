d686085fa70893ae296a7f0e268109da6749e8c4
kavau <kavaunix@proton.me>
2026-06-09 16:02:28 +0200

    Lib: add syslog priority prefixes to log.h

--- lib/log.h
+++ lib/log.h
@@ -14,22 +14,23 @@
 #define LOG_PREFIX "atrium"
 #endif
 
-/* Internal helper macro. */
-#define _log(level, fmt, ...)                                                                      \
-    do {                                                                                           \
-        fprintf(stderr, LOG_PREFIX " [" level "] " fmt "\n", ##__VA_ARGS__);                       \
+/* Internal helper macro. The prio prefix (e.g. "<3>") is parsed by the systemd
+journal to set the PRIORITY field; it is stripped from the message. */
+#define _log(prio, level, fmt, ...)                                               \
+    do {                                                                          \
+        fprintf(stderr, prio LOG_PREFIX " [" level "] " fmt "\n", ##__VA_ARGS__); \
     } while (0)
 
-#define log_info(...) _log("info", __VA_ARGS__)
-#define log_warn(...) _log("warning", __VA_ARGS__)
-#define log_error(...) _log("error", __VA_ARGS__)
+#define log_info(...)  _log("<6>", "info", __VA_ARGS__)
+#define log_warn(...)  _log("<4>", "warning", __VA_ARGS__)
+#define log_error(...) _log("<3>", "error", __VA_ARGS__)
 
 /* Replacement for perror(): appends a description of errno. */
-#define log_syserr(fmt, ...) _log("error", fmt ": %s", ##__VA_ARGS__, strerror(errno))
+#define log_syserr(fmt, ...) _log("<3>", "error", fmt ": %s", ##__VA_ARGS__, strerror(errno))
 
 /* Debug messages (compile-time conditional). */
 #ifdef ATRIUM_DEBUG
-#define log_debug(...) _log("debug", __VA_ARGS__)
+#define log_debug(...) _log("<7>", "debug", __VA_ARGS__)
 #else
 #define log_debug(...) ((void)0)
 #endif
